09cdbf829c0fb218a93fac94d85e0f3e2dcdea23
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-12-03 19:12:20 +0800

    Add files via upload

---
+++ Py#072-3mc005-L-AutoCoding.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 17:22:46 2018
+
+@author: LIZY52
+"""
+
+for i in range(1, 6):             
+    print("\'Value = %d" % i)
+    print("LeftDoubleClick 1")
+    print("Delay 500")
+    print('KeyPress "Num %d", 1' % i)
+    print("Delay 500")
+    print("")
+    print("\'Send")
+    print('KeyPress "Enter", 1')
+    print("Delay 500")
+    print('KeyPress "Enter", 1')
+    print("Delay 5000")
+    print("")
